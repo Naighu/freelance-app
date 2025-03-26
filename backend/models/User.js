9ae49cfa490b4374b3b79ad9971822484553631d
Naigal Roy <naighu@Jobros-MacBook-Air.local>
2025-03-26 13:24:43 +1000

    Updated user model to incoroperate user_type

--- backend/models/User.js
+++ backend/models/User.js
@@ -6,8 +6,7 @@ const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    university: { type: String },
-    address: { type: String },
+    user_type: {type: String, enum : ['client','worker'],required: true}
 });
 
 userSchema.pre('save', async function (next) {
