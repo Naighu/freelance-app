e1d94484efe77738082208ee149d433be0ab3f09
Naigal Roy <naighu@Jobros-MacBook-Air.local>
2025-03-29 15:35:09 +1000

    Sub Task 5.2: Developed the Api to apply for work

--- backend/models/Work.js
+++ backend/models/Work.js
@@ -1,12 +1,19 @@
 
 const mongoose = require('mongoose');
 
+const appliedSchema = new mongoose.Schema({
+    user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    message: { type: String, required: true },
+    amount: { type: Number, required: true }
+  });
+
 const workSchema = new mongoose.Schema({
     title: { type: String, required: true },
     description: { type: String, required: true, unique: true },
     category: { type: String, required: true },
     budget: {type: Number,required: true},
-    user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true } 
+    user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true } ,
+    applied_users: [appliedSchema]
 });
 
 
--- backend/routes/workRoutes.js
+++ backend/routes/workRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const { postWork,addCategory,getCategories,fetchAllWork } = require('../controllers/workController');
-const { validateWorkMiddleware,validateAddCategoryiddleware } = require('../middleware/workMiddleware');
+const { postWork,addCategory,getCategories,fetchAllWork,editWork ,deleteWork,applyWork} = require('../controllers/workController');
+const { validateWorkMiddleware,validateAddCategoryiddleware,validateEditWorkMiddleware,validateDeleteWorkMiddleware,validateApplyWorkMiddleware } = require('../middleware/workMiddleware');
 const { protect,protectAdmin } = require('../middleware/authMiddleware');
 const router = express.Router();
 
@@ -8,6 +8,11 @@ router.post('/post',validateWorkMiddleware,protect, postWork);
 router.post('/category/add',validateAddCategoryiddleware, protectAdmin,addCategory);
 router.get('/category',getCategories);
 router.get('/get',protect,fetchAllWork);
+router.put('/update',protect,validateEditWorkMiddleware,editWork)
+router.delete('/delete',protect,validateDeleteWorkMiddleware,deleteWork)
+router.post('/apply',protect,validateApplyWorkMiddleware,applyWork)
+
+
 
 
 
