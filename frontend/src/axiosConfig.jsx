12cfc1f153a914b59ab344b35d40ac384f206df1
Naigal Roy <naighu@Jobros-MacBook-Air.local>
2025-03-30 21:07:10 +1000

    Updated the baseUrl with production

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5001',
+  // baseURL: 'http://localhost:5001',
+  baseURL: 'http://13.54.187.158:5001',
   headers: { 'Content-Type': 'application/json' },
 });
 
